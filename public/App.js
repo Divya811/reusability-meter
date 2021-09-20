5f9fb3b6283539f9fe65b04b2995ed627f223a93
Divya811 <divyakannan636@gmail.com>
2021-09-20 16:10:56 +0530

    Update App.js

--- public/App.js
+++ public/App.js
@@ -1,44 +1,38 @@
-import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
-import AddTutorial from "./components/add-tutorial.component";
-import ModalWindow from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark Chart">
-          <Link to={"/tutorials"} className="navbar-brand">
-            bezKoder PieChart
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-            <Route exact path="/add" component={AddTutorial} />
-            <Route path="/tutorials/:id" component={Tutorial} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
-}
-
+import React, { useState } from 'react';
+ 
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+ 
+import './App.css';
+ 
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+ 
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap Demo</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
+ 
+const App = () => (
+  <Container className="p-3">
+    <Jumbotron>
+      <h1 className="header">Demonstration Of React Bootstrap</h1>
+      <ExampleToast>
+        Click Here!
+        
+      </ExampleToast>
+    </Jumbotron>
+  </Container>
+);
+ 
 export default App;
